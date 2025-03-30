42e22edfd377f530a1cf56f25468309d22389867
Maruthi Prithivi <maruthi.prithivi@gmail.com>
2025-03-30 21:02:49 +0800

    fixing up github actions

--- agent_zero/monitoring/__init__.py
+++ agent_zero/monitoring/__init__.py
@@ -49,37 +49,37 @@ from .utility import (
 )
 
 __all__ = [
-    # Query Performance
-    "get_current_processes",
-    "get_query_duration_stats",
-    "get_normalized_query_stats",
-    "get_query_kind_breakdown",
-    # Resource Usage
-    "get_memory_usage",
-    "get_cpu_usage",
-    "get_server_sizing",
-    "get_uptime",
-    # Parts & Merges
-    "get_parts_analysis",
-    "get_current_merges",
-    "get_merge_stats",
-    "get_part_log_events",
-    "get_partition_stats",
     # Error Analysis
-    "get_recent_errors",
     "get_error_stack_traces",
+    "get_recent_errors",
     "get_text_log",
     # Insert Operations
     "get_async_insert_stats",
     "get_insert_written_bytes_distribution",
     "get_recent_insert_queries",
+    # Parts & Merges
+    "get_current_merges",
+    "get_merge_stats",
+    "get_part_log_events",
+    "get_partition_stats",
+    "get_parts_analysis",
+    # Query Performance
+    "get_current_processes",
+    "get_normalized_query_stats",
+    "get_query_duration_stats",
+    "get_query_kind_breakdown",
+    # Resource Usage
+    "get_cpu_usage",
+    "get_memory_usage",
+    "get_server_sizing",
+    "get_uptime",
     # System Components
-    "get_mv_query_stats",
     "get_blob_storage_stats",
+    "get_mv_query_stats",
     "get_s3queue_stats",
     # Table Statistics
-    "get_table_stats",
     "get_table_inactive_parts",
+    "get_table_stats",
     # Utility
     "generate_drop_tables_script",
     "get_user_defined_functions",
