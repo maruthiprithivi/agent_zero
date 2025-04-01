99963f2521b891207f5d1fd3f9ed94936b8de37d
Maruthi Prithivi <maruthi.prithivi@gmail.com>
2025-04-01 17:49:03 +0800

    changing back to our original name

--- agent_zero/monitoring/error_analysis.py
+++ agent_zero/monitoring/error_analysis.py
@@ -7,9 +7,10 @@ in a ClickHouse database.
 import logging
 from typing import Any
 
-from agent_one.utils import execute_query_with_retry, log_execution_time
 from clickhouse_connect.driver.client import Client
 from clickhouse_connect.driver.exceptions import ClickHouseError
+
+from agent_zero.utils import execute_query_with_retry, log_execution_time
 
 logger = logging.getLogger("mcp-clickhouse")
 
